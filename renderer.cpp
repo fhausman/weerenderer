aaa27f527259ff3dfbe532bfc0b9cd4a73af6794
Filip Hausman <filip.hausman@gmail.com>
2019-01-30 23:53:56 +0100

    better but still imperfect

--- renderer.cpp
+++ renderer.cpp
@@ -1,21 +1,24 @@
 #include "img/tgaimage.h"
 
-void draw_line(const int x0, const int y0, const int x1, const int y1, TGAImage& image, const TGAColor color = TGAColor{ 255, 255, 255 , 255 })
-{
-    for (float t = 0.; t < 1.; t += 0.01)
-    {
-        auto x = x0 + (x1 - x0)*t;
-        auto y = y0 + (y1 - y0)*t;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
 
+void draw_line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor color = TGAColor{ 255, 255, 255 , 255 })
+{
+    for (auto x = x0; x <= x1; ++x)
+    {
+        int y = static_cast<int>(y0 + (x-x0) * ((y1 - y0) / static_cast<float>(x1 - x0)));
         image.set(x, y, color);
     }
 }
 
 int main(int argc, const char* argv[])
 {
-    TGAImage image{ 800, 800, TGAImage::RGB };
+    TGAImage image{ 100, 100, TGAImage::RGB };
 
-    draw_line(10, 10, 90, 100, image, TGAColor{ 255, 0, 0, 255 });
+    draw_line(13, 20, 80, 40, image, white);
+    draw_line(20, 13, 40, 80, image, red);
+    draw_line(80, 40, 13, 20, image, red);
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
