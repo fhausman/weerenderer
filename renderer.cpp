ee92feb446296f1b5aa5ae3505d5334c45d2a0b7
Filip Hausman <filip.hausman@gmail.com>
2019-05-30 00:07:59 +0200

    wireframe rendering works now with tinyobj

--- renderer.cpp
+++ renderer.cpp
@@ -1,6 +1,8 @@
 #include "img/tgaimage.h"
-#include "obj/obj.hpp"
+#include "tinyobjloader/tiny_obj_loader.h"
 #include "hola/hola.hpp"
+
+#include <iostream>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -35,30 +37,51 @@ void draw_line(const vec2i& v0, const vec2i& v1, TGAImage& image,
 
 int main(int argc, const char* argv[])
 {
-    const int width = 1600;
-    const int height = 1600;
-    TGAImage image{width, height, TGAImage::RGB};
+    TGAImage image{1600, 1600, TGAImage::RGB};
 
-    auto model = Obj::CreateObjModel("head.obj");
-    auto draw_face = [&model, &image](const FaceElements& face) {
-        const auto& face_coords = face.face_element_coords;
-        const auto size = face_coords.size();
-        for (size_t i = 0; i < size; ++i)
-        {
-            const auto v0 =
-                model.GetVertexIndicesAt(face_coords[i].vertex_indices);
-            const auto v1 = model.GetVertexIndicesAt(
-                face_coords[(i + 1) % size].vertex_indices);
-            int x0 = (int)((v0.x + 1.) * image.get_width() / 2.);
-            int y0 = (int)((v0.y + 1.) * image.get_height() / 2.);
-            int x1 = (int)((v1.x + 1.) * image.get_width() / 2.);
-            int y1 = (int)((v1.y + 1.) * image.get_height() / 2.);
-            draw_line({x0, y0}, {x1, y1}, image);
-        }
+    auto objreader = tinyobj::ObjReader{};
+    objreader.ParseFromFile("head.obj");
+
+    if (!objreader.Valid())
+    {
+        std::cerr << "Invalid obj file, aborting...";
+        return -1;
+    }
+
+    const auto& head = objreader.GetShapes().front();
+    const auto& indices = head.mesh.indices;
+    const auto& face_vertices = head.mesh.num_face_vertices;
+    const auto& vertices = objreader.GetAttrib().vertices;
+
+    const auto get_vertex = [&vertices](const auto idx) {
+        const auto vertex_start = idx * 3;
+        return vec2f{ vertices[vertex_start], vertices[vertex_start + 1] };
     };
 
-    const auto& faces = model.GetFaceElements();
-    std::for_each(faces.begin(), faces.end(), draw_face);
+    const auto get_next_index = [](const auto face_idx, const auto face_size) {
+        return (face_idx + 1) % face_size;
+    };
+
+    const auto calc_img_coord = [](const auto obj_coord, const auto image_dimension) {
+        return static_cast<int>((obj_coord + 1.) * image_dimension / 2.);
+    };
+
+    size_t indices_idx = 0;
+    for (size_t i = 0; i < face_vertices.size(); ++i)
+    {
+        const auto face_size = face_vertices[i];
+        const auto face = std::vector<tinyobj::index_t>(indices.begin() + indices_idx, indices.begin() + indices_idx + face_size);
+        for (size_t fi = 0; fi < face_size; ++fi)
+        {
+            const auto v0 = get_vertex(face[fi].vertex_index);
+            const auto v1 = get_vertex(face[get_next_index(fi, face_size)].vertex_index);
+            const auto p0 = vec2i{ calc_img_coord(x(v0), image.get_width()), calc_img_coord(y(v0), image.get_height())};
+            const auto p1 = vec2i{ calc_img_coord(x(v1), image.get_width()), calc_img_coord(y(v1), image.get_height())};
+
+            draw_line(p0, p1, image);
+        }
+        indices_idx += face_size;
+    }
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
