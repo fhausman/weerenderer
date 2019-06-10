fa591322f289a712223b64c5c06fa9338587fd40
Filip Hausman <filip.hausman@gmail.com>
2019-06-11 00:04:20 +0200

    restructuring code

--- renderer.cpp
+++ renderer.cpp
@@ -1,90 +1,36 @@
-#include "img/tgaimage.h"
-#include "tinyobjloader/tiny_obj_loader.h"
-#include "hola/hola.hpp"
+#include "renderer.hpp"
 
-#include <iostream>
-
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
-const TGAColor blue = TGAColor(0, 0, 255, 255);
-
-using namespace hola;
-
-void draw_line(const vec2i& v0, const vec2i& v1, TGAImage& image,
-    const TGAColor color = TGAColor{255, 255, 255, 255})
+BoundingBox calculate_bounding_box(const Triangle& triangle, const ScreenResolution& screen)
 {
-    const auto dx = std::abs(get_x(v1) - get_x(v0));
-    const auto dy = std::abs(get_y(v1) - get_y(v0));
-    const auto steps_nr = dx >= dy ? dx : dy;
+    vec bbmax{ 0, 0 };
+    vec bbmin{ screen.width, screen.height };
+    vec clamp{ screen.width, screen.height };
 
-    const auto xi = get_x(v0) > get_x(v1) ? -1 : 1;
-    const auto yi = get_y(v0) > get_y(v1) ? -1 : 1;
-
-    const auto calculate_next_pixel =
-        [steps_nr](const auto start, const auto difference, const auto step) {
-            return static_cast<int>(
-                start + step * (difference / static_cast<float>(steps_nr)));
-        };
-
-    for (size_t step = 0; step < steps_nr; ++step)
+    for (const auto& vertex : triangle)
     {
-        const auto px_x = calculate_next_pixel(get_x(v0), dx * xi, step);
-        const auto px_y = calculate_next_pixel(get_y(v0), dy * yi, step);
-        image.set(px_x, px_y, color);
+        for (size_t i = 0; i < vertex.size(); ++i)
+        {
+            bbmin[i] = std::max(0, std::min(bbmin[i], vertex[i]));
+            bbmax[i] = std::min(clamp[i], std::max(bbmax[i], vertex[i]));
+        }
     }
+
+    return { bbmin, bbmax };
 }
 
-int main(int argc, const char* argv[])
+std::optional<vec3f> calculate_barycentric(const Point& p, const Triangle& triangle)
 {
-    TGAImage image{1600, 1600, TGAImage::RGB};
+    const auto is_degenerate = [](const auto& u) { return std::abs(get_z(u)) < 0; };
 
-    auto objreader = tinyobj::ObjReader{};
-    objreader.ParseFromFile("head.obj");
+    const auto&[a, b, c] = triangle;
+    const vec3f x_part = convert_to<vec3f>(vec3i{ get_x(c) - get_x(a), get_x(b) - get_x(a), get_x(a) - get_x(p) });
+    const vec3f y_part = convert_to<vec3f>(vec3i{ get_y(c) - get_y(a), get_y(b) - get_y(a), get_y(a) - get_y(p) });
+    const auto u = cross(x_part, y_part);
 
-    if (!objreader.Valid())
+    if (is_degenerate(u))
     {
-        std::cerr << "Invalid obj file, aborting...";
-        return -1;
+        return std::nullopt;
     }
 
-    const auto& head = objreader.GetShapes().front();
-    const auto& indices = head.mesh.indices;
-    const auto& face_vertices = head.mesh.num_face_vertices;
-    const auto& vertices = objreader.GetAttrib().vertices;
-
-    const auto get_vertex = [&vertices](const auto idx) {
-        const auto vertex_start = idx * 3;
-        return vec2f{ vertices[vertex_start], vertices[vertex_start + 1] };
-    };
-
-    const auto get_next_index = [](const auto face_idx, const auto face_size) {
-        return (face_idx + 1) % face_size;
-    };
-
-    const auto calc_img_coord = [](const auto obj_coord, const auto image_dimension) {
-        return static_cast<int>((obj_coord + 1.) * image_dimension / 2.);
-    };
-
-    size_t indices_idx = 0;
-    for (size_t i = 0; i < face_vertices.size(); ++i)
-    {
-        const auto face_size = face_vertices[i];
-        const auto face = std::vector<tinyobj::index_t>(indices.begin() + indices_idx, indices.begin() + indices_idx + face_size);
-        for (size_t fi = 0; fi < face_size; ++fi)
-        {
-            const auto v0 = get_vertex(face[fi].vertex_index);
-            const auto v1 = get_vertex(face[get_next_index(fi, face_size)].vertex_index);
-            const auto p0 = vec2i{ calc_img_coord(get_x(v0), image.get_width()), calc_img_coord(get_y(v0), image.get_height())};
-            const auto p1 = vec2i{ calc_img_coord(get_x(v1), image.get_width()), calc_img_coord(get_y(v1), image.get_height())};
-
-            draw_line(p0, p1, image);
-        }
-        indices_idx += face_size;
-    }
-
-    image.flip_vertically();
-    image.write_tga_file("output.tga");
-
-    return 0;
+    return vec3f{ 1.f - (get_x(u) + (get_y(u))) / get_z(u), get_y(u) / get_z(u), get_x(u) / get_z(u) };
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,39 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include <optional>
+#include "../renderer.hpp"
 #include "../hola/hola.hpp"
-
-using namespace hola;
-
-using BoundingBox = std::tuple<vec2i, vec2i>;
-using Triangle = std::array<vec2i, 3>;
-using Point = vec2i;
-
-struct ScreenResolution
-{
-    int width;
-    int height;
-};
-
-BoundingBox get_bounding_box(
-    const Triangle& triangle, const ScreenResolution& screen)
-{
-    vec bbmax{0, 0};
-    vec bbmin{screen.width, screen.height};
-    vec clamp{screen.width, screen.height};
-
-    for (const auto& vertex : triangle)
-    {
-        for (size_t i = 0; i < vertex.size(); ++i)
-        {
-            bbmin[i] = std::max(0, std::min(bbmin[i], vertex[i]));
-            bbmax[i] = std::min(clamp[i], std::max(bbmax[i], vertex[i]));
-        }
-    }
-
-    return {bbmin, bbmax};
-}
 
 SCENARIO("Bounding box calculation", "[renderer]")
 {
@@ -42,8 +11,8 @@ SCENARIO("Bounding box calculation", "[renderer]")
         const Triangle triangle = {{{10, 30}, {15, 40}, {20, 20}}};
         WHEN("calculating bounding box")
         {
-            const auto [bbmin, bbmax] =
-                get_bounding_box(triangle, {1980, 1080});
+            auto [bbmin, bbmax] =
+                calculate_bounding_box(triangle, {1980, 1080});
             THEN("bounding box should consist furthest triangle coordinates")
             {
                 REQUIRE(bbmin == vec{10, 20});
@@ -57,8 +26,8 @@ SCENARIO("Bounding box calculation", "[renderer]")
         const Triangle triangle = {{{-10, -30}, {40, 40}, {-20, -50}}};
         WHEN("calculating bounding box")
         {
-            const auto [bbmin, bbmax] =
-                get_bounding_box(triangle, {1980, 1080});
+            auto [bbmin, bbmax] =
+                calculate_bounding_box(triangle, {1980, 1080});
             THEN("bounding box should be clamped to screen resolution")
             {
                 REQUIRE(bbmin == vec{0, 0});
@@ -72,8 +41,8 @@ SCENARIO("Bounding box calculation", "[renderer]")
         const Triangle triangle = {{{-10, -30}, {-40, -100}, {-20, -50}}};
         WHEN("calculating bounding box")
         {
-            const auto [bbmin, bbmax] =
-                get_bounding_box(triangle, {1980, 1080});
+            auto [bbmin, bbmax] =
+                calculate_bounding_box(triangle, {1980, 1080});
             THEN("bounding box should be zeroed")
             {
                 REQUIRE(bbmin == vec{0, 0});
@@ -87,8 +56,8 @@ SCENARIO("Bounding box calculation", "[renderer]")
         const Triangle triangle = {{{0, 0}, {1980, 1080}, {0, 1080}}};
         WHEN("calculating bounding box")
         {
-            const auto [bbmin, bbmax] =
-                get_bounding_box(triangle, {1980, 1080});
+            auto [bbmin, bbmax] =
+                calculate_bounding_box(triangle, {1980, 1080});
             THEN("bounding box should be fullscreen")
             {
                 REQUIRE(bbmin == vec{0, 0});
@@ -96,36 +65,6 @@ SCENARIO("Bounding box calculation", "[renderer]")
             }
         }
     }
-}
-
-std::optional<vec3f> calculate_barycentric(
-    const Point& p, const Triangle& triangle)
-{
-    const auto is_degenerate = [](const auto& u) { return std::abs(get_z(u)) < 0; };
-
-    const auto& [a, b, c] = triangle;
-    const vec3f x_part = convert_to<vec3f>(vec3i{ get_x(c) - get_x(a), get_x(b) - get_x(a), get_x(a) - get_x(p) });
-    const vec3f y_part = convert_to<vec3f>(vec3i{ get_y(c) - get_y(a), get_y(b) - get_y(a), get_y(a) - get_y(p) });
-    const auto u = cross(x_part, y_part);
-
-    if (is_degenerate(u))
-    {
-        return std::nullopt;
-    }
-
-    return vec3f{1.f - (get_x(u) + (get_y(u)))/get_z(u), get_y(u)/get_z(u), get_x(u)/get_z(u)};
-}
-
-bool is_inside_triangle(const Point& p, const Triangle& triangle)
-{
-    if (auto barycentric = calculate_barycentric(p, triangle); barycentric)
-    {
-        return get_x(*barycentric) < 0.f ||
-            get_y(*barycentric) < 0.f ||
-            get_z(*barycentric) < 0.f;
-    }
-
-    return false;
 }
 
 SCENARIO("Calculating point barycentric coordinates in relation to triangle", "[renderer]")
