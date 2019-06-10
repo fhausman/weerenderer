c065227f1a1bc1c28255068399b7b08bcbc86326
Filip Hausman <filip.hausman@gmail.com>
2019-06-10 23:42:45 +0200

    tests for calculating barycentric

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -104,8 +104,8 @@ std::optional<vec3f> calculate_barycentric(
     const auto is_degenerate = [](const auto& u) { return std::abs(get_z(u)) < 0; };
 
     const auto& [a, b, c] = triangle;
-    const vec3f x_part = convert_to<vec3f>(vec3i{ get_x(b) - get_x(a), get_x(c) - get_x(a), get_x(p) - get_x(a) });
-    const vec3f y_part = convert_to<vec3f>(vec3i{ get_y(b) - get_y(a), get_y(c) - get_y(a), get_y(p) - get_y(a) });
+    const vec3f x_part = convert_to<vec3f>(vec3i{ get_x(c) - get_x(a), get_x(b) - get_x(a), get_x(a) - get_x(p) });
+    const vec3f y_part = convert_to<vec3f>(vec3i{ get_y(c) - get_y(a), get_y(b) - get_y(a), get_y(a) - get_y(p) });
     const auto u = cross(x_part, y_part);
 
     if (is_degenerate(u))
@@ -113,17 +113,65 @@ std::optional<vec3f> calculate_barycentric(
         return std::nullopt;
     }
 
-    return vec3f{1 - (get_x(u) + (get_y(u)))/get_z(u), get_y(u)/get_z(u), get_x(u)/get_z(u)};
+    return vec3f{1.f - (get_x(u) + (get_y(u)))/get_z(u), get_y(u)/get_z(u), get_x(u)/get_z(u)};
 }
 
 bool is_inside_triangle(const Point& p, const Triangle& triangle)
 {
     if (auto barycentric = calculate_barycentric(p, triangle); barycentric)
     {
-        return get_x(*barycentric) < 0||
-            get_y(*barycentric) < 0 ||
-            get_z(*barycentric) < 0;
+        return get_x(*barycentric) < 0.f ||
+            get_y(*barycentric) < 0.f ||
+            get_z(*barycentric) < 0.f;
     }
 
     return false;
+}
+
+SCENARIO("Calculating point barycentric coordinates in relation to triangle", "[renderer]")
+{
+    GIVEN("non-degenerate triangle")
+    {
+        const Triangle triangle = {{{10,10}, {50,10}, {30,50}}};
+        WHEN("P is point on one of the triangle vertices")
+        {
+            const Point p1{ 10,10 };
+            const Point p2{ 50,10 };
+            const Point p3{ 30,50 };
+
+            const auto bar1 = calculate_barycentric(p1, triangle);
+            const auto bar2 = calculate_barycentric(p2, triangle);
+            const auto bar3 = calculate_barycentric(p3, triangle);
+            THEN("barycentric of P is unit vector")
+            {
+                REQUIRE(bar1 == vec{ 1.f, 0.f, 0.f });
+                REQUIRE(bar2 == vec{ 0.f, 1.f, 0.f });
+                REQUIRE(bar3 == vec{ 0.f, 0.f, 1.f });
+            }
+        }
+
+        WHEN("P is inside triangle")
+        {
+            const Point p{ 30, 10 };
+
+            const auto bar = *calculate_barycentric(p, triangle);
+            THEN("all barycentric coords are positive")
+            {
+                REQUIRE(get_x(bar) >= 0.f);
+                REQUIRE(get_y(bar) >= 0.f);
+                REQUIRE(get_z(bar) >= 0.f);
+            }
+        }
+
+        WHEN("P is outside triangle")
+        {
+            const Point p{ 100, 100 };
+
+            const auto bar = *calculate_barycentric(p, triangle);
+            THEN("one of barycentric coords is negative")
+            {
+                REQUIRE((get_x(bar) < 0.f || get_y(bar) < 0.f || get_z(bar) < 0.f));
+            }
+        }
+    }
 }
