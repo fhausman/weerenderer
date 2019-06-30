ef6ccef650e259fa40044c9cd785ce25a96d429b
Filip Hausman <filip.hausman@gmail.com>
2019-06-30 16:59:31 +0200

    image interfaces

--- img.hpp
+++ img.hpp
@@ -22,3 +22,28 @@ Color get_pixel(Image& image, const int32_t x, const int32_t y);
 Image read_image(const std::filesystem::path& path_to_img);
 
 void write_image(Image& image, const std::filesystem::path& path_to_write);
+
+struct RGBA
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+};
+
+struct IColor
+{
+    virtual RGBA ToRgba() const = 0;
+    virtual ~IColor() = default;
+};
+
+struct IImg
+{
+    virtual void CreateImage(const Width width, const Height height) = 0;
+    virtual void ReadImage(const std::filesystem::path& path_to_img) = 0;
+    virtual void WriteImage(const std::filesystem::path& path_to_write) const = 0;
+    virtual std::tuple<Width, Height> GetImageSize() const = 0;
+    virtual const IColor& GetPixelColor(const int32_t x, const int32_t y) const = 0;
+    virtual void SetPixelColor(const int32_t x, const int32_t y, const float_t intensity, const IColor& color) = 0;
+    virtual ~IImg() = default;
+};
